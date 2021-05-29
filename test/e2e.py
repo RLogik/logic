06be312907e7ad1d583494fc20afc4a062d9175f
RLogik <r.bitlogik@gmail.com>
2021-05-29 09:17:39 +0200

    master > master: test

---
+++ test/e2e.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# IMPORTS
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+import sys;
+sys.tracebacklimit = 0;
+
+from src.core.log import logInfo;
+from src.core.log import logWarn;
+from src.core.log import logError;
+from src.core.log import logFatal;
+from src.core.utils import getAttribute;
+from src.core.utils import readConfig;
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# GLOBAL VARIABLE
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+PATH_CONFIG: str;
+CONFIG: dict;
+TESTCASES: list;
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# MAIN METHOD
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+def main():
+    setup(*sys.argv[1:]);
+    for testcase in TESTCASES:
+        runTests(testcase);
+    return;
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# SECONDARY METHODS
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+def setup(path: str, *_):
+    global PATH_CONFIG;
+    global CONFIG;
+    global TESTCASES;
+    PATH_CONFIG = path;
+    CONFIG = readConfig(PATH_CONFIG);
+    TESTCASES = getAttribute(CONFIG, 'parts', 'test');
+    return;
+
+def runTests(testcase: dict):
+    logWarn('Not yet implemented');
+    return;
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# EXECUTION
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+if __name__ == '__main__':
+    main();
